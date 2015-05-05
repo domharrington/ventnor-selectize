28db35761ea4d80677e313f11c58081fcd41ee71
Dom Harrington <dom@harrington-mail.com>
2015-05-05 17:59:39 +0100

    Make sure logger is set

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function VentnorSelectize(serviceLocator, selected, options) {
   this.$el = $('<select multiple/>')
   if (!options || !options.service) throw new Error('no service provided')
   this.service = options.service
+  this.logger = options.logger || serviceLocator.logger || console
 
   this.$el.addClass('control control--choice control--multiline')
   this.el = this.$el[0]
@@ -20,7 +21,7 @@ VentnorSelectize.prototype = Object.create(View.prototype)
 
 VentnorSelectize.prototype.initialize = function () {
   this.service.find('', { _id: { $in: this.selected } }, [], {}, function (err, res) {
-    if (err) return this.serviceLocator.logger.error('Cannot find existing items')
+    if (err) return this.logger.error('Cannot find existing items')
 
     res.results.forEach(function (model) {
       // The item needs to be added to the list
@@ -41,7 +42,7 @@ VentnorSelectize.prototype.updateSelection = function () {
 VentnorSelectize.prototype.load = function (query, cb) {
 
   this.service.find(query, {}, [], {}, function (err, data) {
-    if (err) return this.serviceLocator.logger.error('Cannot load new items')
+    if (err) return this.logger.error('Cannot load new items')
     cb(data.results.map(function (model) {
       return { value: model._id, text: model.name }
     }))
